1c1b1dd86c5cdc626c4393c3e2c224854d4d3402
filreg <reggimenti.filippo@gmail.com>
2014-07-02 12:41:32 +0200

    added contro class Profile

---
+++ Classes/Control/Profile.php
@@ -0,0 +1,55 @@
+<?php
+/**
+*this file contains the Profile class,
+*
+*/
+
+namespace Control;
+
+global $projectDirectory;
+
+require_once $projectDirectory.'/Classes/View/Navigation.php';
+require_once $projectDirectory.'/Classes/Foundation/Resource.php';
+require_once $projectDirectory.'/Classes/Entity/Resource.php';
+require_once $projectDirectory.'/Classes/Foundation/Subject.php';
+require_once $projectDirectory.'/Classes/Utility/Singleton.php';
+require_once $projectDirectory.'/Classes/Foundation/User.php';
+
+/**
+ * it is the profile control class,
+ *
+ *
+ */
+class Profile
+{
+	public function __construct()
+	{	
+		$this->controlProfile();
+	}
+	
+	public function controlProfile(){
+		$userSession = \Utility\Singleton::getInstance("\Control\Session");
+		$username=$userSession->get('username');
+		$view = \Utility\Singleton::getInstance("\View\Home");
+		$this->setUserInformations($view,$username);
+		$this->setResourcesUploaded($view,$username);
+		$view->display('profile.tpl');
+	}
+	
+	public function setUserInformations($view,$username){
+		$userDb=new \Foundation\User();
+		$user=$userDb->getByUsername($username);
+		$view->assign('username',$user->getUsername());
+		$view->assign('name',$user->getName());
+		$view->assign('surname',$user->getsurname());
+		$view->assign('email',$user->getEmail());
+		$view->assign('degreeCourse',$user->getDegreeCourse());
+	}
+	
+	public function setResourcesUploaded($view,$username){
+		$resourceDb=new \Foundation\Resource();
+		$resources=$resourceDb-> getResourcesByUser($username);
+		$view->assign('resource',$resources);
+		
+	}
+}
