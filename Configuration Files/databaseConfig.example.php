cf9199a3e6056305f4043a0e95f413648f834325
filreg <reggimenti.filippo@gmail.com>
2014-07-02 12:47:50 +0200

    added configuration class for the database

---
+++ Configuration Files/databaseConfig.example.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+
+global $mysqlConfig;
+
+$mysqlConfig['user'] = 'root';
+$mysqlConfig['password'] = 'your password';
+$mysqlConfig['host'] = 'your host';
+$mysqlConfig['database'] = 'database name';
+
