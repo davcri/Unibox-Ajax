b2e0bf693e6c1df7cec8eaac9aea1d3bdf410d2c
Davide Cristini <davcri91@gmail.com>
2014-07-03 10:45:51 +0200

    Implementing ajax upload.

--- Smarty_dir/templates/javascript/upload.js
+++ Smarty_dir/templates/javascript/upload.js
@@ -2,47 +2,34 @@
 $(function(){
 
 	$("#uploadForm").submit(function(event) {
-		event.preventDefault();
+		event.preventDefault();	
+
+		var formData = new FormData(this);
+
+		// for details and explanation read: http://stackoverflow.com/questions/5392344/sending-multipart-formdata-with-jquery-ajax 
+		// processData and contentType need to be false, otherwise the ajax call will fail.
+		$.ajax({
+			url: 'index.php?controllerAction=upload',
+			type: 'POST',
+			data: new FormData(this),
+			processData: false,
+			contentType: false
+		}).done(function(data){
+			changePage(data);
+		});
 	});
 
 	var degreeCourse;
-
-	var uploadObj = $("#fileuploader").uploadFile({
-		url:"index.php",
-		multiple:false,
-		maxFileCount:1,
-		showFileCounter:false,
-		fileName:"file",
-		autoSubmit:false
-	});
 
 	$("#degreeCourse").change(function(){
 		var selectField = $("#subject");
 		selectField.empty();
 		degreeCourse = $(this).val();
 
-		$.get("index.php?controllerAction=upload&uploadAction="+degreeCourse, function(data){
+		$.get("index.php?controllerAction=upload&uploadAction=" + degreeCourse, function(data){
 			data.forEach(function(element){
 				selectField.append("<option>" + element + "</option>");
 			});				
 		},"json");			
 	});	
-
-	$("#uploadButton").click(function(event){
-	
-		var file = document.getElementById('inputFile');
-
-		console.log(uploadObj);
-		
-		var formData = {
-			"name": $("#name").val(),
-			"category": $("#category").val(),
-			"degreeCourse": $("#degreeCourse").val(),
-			"subject": $("#subject").val(),
-		};
-
-		/*$.post("index.php?controllerAction=upload", formData, function(data){
-			changePage(data);
-		});*/
-	});
 });
--- Smarty_dir/templates/upload.tpl
+++ Smarty_dir/templates/upload.tpl
@@ -1,13 +1,14 @@
 
 {if $loggedIn}
-  <div id="uploadForm" class="jumbotron">
+  <div class="mainContent">
+    <br>
     <p class="lead">Da questa pagina puoi inviarci i tuoi file.</p>
     <p>Ti basta compilare la form qui sotto</p> <br>
 
-    <form name="uploadForm" method="POST" enctype="multipart/form-data" action="index.php?controllerAction=upload">
+    <form id="uploadForm" method="POST" enctype="multipart/form-data" action="index.php?controllerAction=upload">
       <div class="form-group">
         <label>Nome risorsa</label>
-        <input type="text" class="form-control" id="name" placeholder="Nome risorsa">
+        <input type="text" class="form-control" id="name" placeholder="Nome risorsa" name="name">
       </div>
 
       <div class="form-group">
@@ -40,8 +41,7 @@
       <div class="form-group">
         <p class="help-block">Seleziona il file da caricare</p>
         <label>File input</label>
-        <div id="fileuploader">Upload</div>
-        <!-- <input type="file" id="inputFile"> -->
+        <input type="file" id="inputFile" name="file">
       </div>
 
       <button id="uploadButton"  class="btn btn-primary">Invia</button>
@@ -51,7 +51,4 @@
   <div id="loginRequired" class="alert alert-danger text-center">Devi registrarti o effettuare il login per caricare file.</div>
 {/if}
 
-  
-<script src="Smarty_dir/templates/javascript/jquery.uploadfile.min.js"></script>
 <script src="Smarty_dir/templates/javascript/upload.js"></script>
-<link href="Smarty_dir/templates/css/uploadfile.min.css" rel="stylesheet">
