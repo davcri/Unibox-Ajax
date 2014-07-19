78143d527318c40b9e1c3b5c68e50d58bea80b24
Davide <davcri91@gmail.com>
2014-07-19 22:29:32 +0200

    Password hash now works on PHP 5.3

--- Classes/Control/Main.php
+++ Classes/Control/Main.php
@@ -93,9 +93,6 @@ class Main
 				break;
 				
 			default:
-				/*$home = new \Control\Home();
-				print $home->controlHome();
-				*/
 				$resourceDb = new \Foundation\Resource();
 				$greatestUsers = $resourceDb->getMostActiveUsers();
 				$mainView->assign("greatestUsers",$greatestUsers);
