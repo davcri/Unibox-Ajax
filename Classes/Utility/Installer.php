6d19f0bd40eaaa1c0169c27d6a33945b29625177
Davide <davcri91@gmail.com>
2014-07-26 14:01:09 +0200

    Updated phpdoc.

--- Classes/Utility/Installer.php
+++ Classes/Utility/Installer.php
@@ -139,17 +139,20 @@ class Installer
 	}
 	
 	/**
+	 * Gets installation form.
 	 * 
+	 * If $errorMessage contains a string, the installPage.tpl will display an error. If $errorMessage 
+	 * is false or an empty string, the form is displayed without errors.
 	 * 
-	 * @param bool $displayErrorMessage
+	 * @param string $errorMessage 
 	 * @return string Rendered template.
 	 */
 	public function getForm($errorMessage)
 	{	
 		$installPage = \Utility\Singleton::getInstance("\View\Main");
 		
-		//if(!empty($errorMessage))
-			$installPage->assign('errorMessage', $errorMessage); //enable error message
+		
+		$installPage->assign('errorMessage', $errorMessage); //enable error message
 		
 		$installPage->assign("projectDetails", $this->projectDetails);
 		$installPage->assign("serverDetails", $this->serverDetails);
@@ -246,6 +249,13 @@ class Installer
 		}
 	}
 	
+	/**
+	 * Tests the database configuration.
+	 * 
+	 * Gets the form's values and tries to ping the database to test the configuration.
+	 * 
+	 * @return bool true on success, false on failure
+	 */
 	public function testConfig()
 	{		
 		$installPage = \Utility\Singleton::getInstance("\View\Main");
@@ -261,8 +271,13 @@ class Installer
 	}
 	
 	/**
+	 * Runs the sql installation script.
+	 * 
+	 * The installation script selected is the one contained in project root with the name $this->databaseScriptName.
+	 * (default is $databaseScriptName = "Unibox_install.sql")
 	 * 
 	 * @todo review this method
+	 * @return bool true on success, false on failure
 	 */
 	public function installDatabase()
 	{
